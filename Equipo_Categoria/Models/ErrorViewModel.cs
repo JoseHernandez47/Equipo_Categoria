285be1b3283acb793a638d15ebf2987d05d9bba1
PC <PC@DESKTOP-EF1AUHA>
2024-02-21 18:49:24 -0600

    Agregar archivos de proyecto.

---
+++ Equipo_Categoria/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Equipo_Categoria.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
